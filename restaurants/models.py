10a121763f1d859248d07d1c18d14f97ee66e8f9
Sana AlKhaled <sanaalkhaled@Sanas-MacBook-Pro.local>
2020-01-29 14:56:11 +0300

    Task8Done

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Restaurant(models.Model):
+    image = models.ImageField(null=True,blank=True)
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
